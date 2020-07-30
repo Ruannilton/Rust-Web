bfd8eb1b30b01f3420f8c1f32a6546c7d29a4fcf
Ruannilton <ruannilton@gmail.com>
2020-07-30 20:21:25 -0300

    some html

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(decl_macro)]
 
 use rocket::{self, get, routes};
+use std::fs::File;
 
 #[get("/")]
 fn root() -> &'static str {
@@ -17,8 +18,14 @@ fn print_name(name: String) -> String {
     format!("Hello {}", name)
 }
 
+#[get("/")]
+fn index_page() -> File {
+    File::open("pages/index.html").expect("File not found")
+}
+
 fn main() {
     rocket::ignite()
         .mount("/", routes![root, hello_world, print_name])
+        .mount("/pages", routes![index_page])
         .launch();
 }
